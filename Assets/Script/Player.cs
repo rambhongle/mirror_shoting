33565fe69b93c0ab0b9bf2bd889f431cc350fdc8
Ram Bhongle <bhongleram@gmail.com>
2020-12-01 12:32:06 +0530

    First Commit

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+     [SerializeField] float _speed = 1f;
+     //[SerializeField] Transform _direction;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
